64d02aea06dec2451f9056f7564fc29cb1eb4615
hellogaon <la_vega_@naver.com>
2017-04-07 18:42:27 +0900

    [20170407] DB Test

--- Project#1/note-app/src/index.js
+++ Project#1/note-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const memoElement = document.getElementById('memo');
+const memoElement = document.getElementById('root');
 ReactDOM.render(<App />, memoElement);
 
 if (module.hot) {
